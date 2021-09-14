1635c1c8ef1538377324033e11a7351a9393fd9b
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-09-13 22:17:19 -0300

    Nova correção de aluno
    UC9 - SA2 - Atividade 2 - Jamile, Thiago

---
+++ UC9/SA2 - Atividade 2 - Presencial/Alunos/Thiago Francis/spec/spec_cartoleta.js
@@ -0,0 +1,45 @@
+// DESCRIBE - conjunto de teste
+// IT - caso de teste
+// EXPECT- asserção - pergunta sobre o que está sendo enviado e recebido
+// exemplo. expect(10).toBe(10)  
+// **********************************************************************************
+// Matchers - pontos para perguntar sobre o expect
+// toBe, toBeGreaterThan , not.toBe , not.toBeGreaterThan
+// No livro digital página e no material web página 152 SA2 Etapa 2 página 100
+// Saiba mais sobre matchers em https://imasters.com.br/front-end/jasmine-entendendo-os-matchers
+//toBe : O que estou passando é igual ao retorno esperado.
+//.not.toBe : Quando o que estou passando não é igual ao retorno esperado.
+// toBeLessThan: Valor passado é menor do que o retorno esperado
+//toBeGreaterThan: Valor passado é maior do que o retorno esperado
+
+
+
+
+describe("Teste de validação valor_do_jogo ", function() {
+
+  it("Valores Sorteio do Placar ", function() {
+	expect( sorteio() ).toBeGreaterThan(-1);	
+	expect( sorteio() ).toBeLessThan(5);	
+  });
+});
+
+
+describe("Teste de validação validar_pontos ", function() {
+
+  it("Validar Campeões ", function() {
+	expect( validar_pontos(1,3) ).toBe("Vitória do Time 2");
+	expect( validar_pontos(3,1) ).toBe("Vitória do Time 1");
+	expect( validar_pontos(1,1) ).toBe("Empate");
+  });
+});
+ 
+
+
+describe("Teste de validação validar_pontos INVALIDOS", function() {
+
+  it("Validar Campeões ", function() {
+	expect( validar_pontos(3,1) ).not.toBe("Vitória do Time 2");		
+	expect( validar_pontos(1,3) ).not.toBe ("Vitória do Time 1");	
+	expect( validar_pontos(3,1) ).not.toBe("Empate"); 
+  });
+});
